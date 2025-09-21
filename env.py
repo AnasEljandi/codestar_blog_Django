3b11962292767b274fa3fe11f5f4e390fc5e82fc
AnasEljandi <anas.eljandi.110@gmail.com>
2025-09-21 18:38:50 +0100

    creating database

---
+++ env.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL",
+    "postgresql://neondb_owner:npg_byV6tofrMSY1@ep-solitary-mountain-agocnhhs.c-2.eu-central-1.aws.neon.tech/smirk_repel_juicy_448142"
+)
