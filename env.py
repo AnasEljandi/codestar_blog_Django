96a321309855d5715149be1534f2636070d220ee
AnasEljandi <anas.eljandi.110@gmail.com>
2025-09-22 21:26:04 +0100

    Store images on Cloudinary

--- env.py
+++ env.py
@@ -5,3 +5,6 @@ os.environ.setdefault(
     "postgresql://neondb_owner:npg_byV6tofrMSY1@ep-solitary-mountain-agocnhhs.c-2.eu-central-1.aws.neon.tech/smirk_repel_juicy_448142"
 )
 os.environ.setdefault("SECRET_KEY", "<your_choice_of_secret_key>")
+os.environ.setdefault(
+    "CLOUDINARY_URL", "cloudinary://234261296217868:ZKDk2roYFnDh_oYGHI2DCB2_gdot@dtblf2tm2"
+)
