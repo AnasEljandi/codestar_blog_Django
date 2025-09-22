1a2c4dbf2eb2e773c4714f17b3583a046554bb06
AnasEljandi <anas.eljandi.110@gmail.com>
2025-09-22 13:28:50 +0100

    build  blog post view redo 2

--- blog/views.py
+++ blog/views.py
@@ -3,12 +3,10 @@ from django.views import generic
 from .models import Post
 
 # Create your views here.
-
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
-paginate_by = 6
-
+    paginate_by = 6
 
 
 def post_detail(request, slug):
